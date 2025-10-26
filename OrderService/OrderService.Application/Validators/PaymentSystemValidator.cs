63c58e1532aed738050b405cd6378f0310484803
Ziya Hacili <magahajili@gmail.com>
2025-10-26 11:43:44 +0400

    validator

---
+++ OrderService/OrderService.Application/Validators/PaymentSystemValidator.cs
@@ -0,0 +1,39 @@
+﻿using OrderService.Core.Interfaces;
+using OrderService.Shared.Dtos.PaymentDtos;
+using FluentValidation;
+
+namespace OrderService.Application.Validators;
+
+public class PaymentSystemValidator : AbstractValidator<PaymentRequestDto>
+{
+    private readonly IPaymentService _paymentService;
+
+    public PaymentSystemValidator(IPaymentService paymentService)
+    {
+        _paymentService = paymentService;
+
+        RuleFor(x => x.CardNumber)
+            .NotEmpty()
+            .WithMessage("Card number is required")
+            .Matches(RegexPatterns.cardNumberPattern)
+            .WithMessage("Valid Card Number format is required");
+
+        RuleFor(x => x.CVV)
+            .NotEmpty()
+            .WithMessage("CVV is required")
+            .Matches(RegexPatterns.cvvPattern)
+            .WithMessage("Valid CVV is required");
+
+        RuleFor(x => x.ExpirationDate)
+            .NotEmpty()
+            .WithMessage("Expiration Date is required")
+            .Matches(RegexPatterns.expirationDatePattern)
+            .WithMessage("Valid Expiration Date is required");
+
+        RuleFor(x => x.CardHolderName)
+            .NotEmpty()
+            .WithMessage("Card Holder Name is required")
+            .Matches(RegexPatterns.cardHolderNamePattern)
+            .WithMessage("Valid Card Holder Name is required");
+    }
+}
---
+++ OrderService/OrderService.Application/Validators/RegexPatterns.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderService.Application.Validators;
+
+public class RegexPatterns
+{
+    public const string cardNumberPattern = @"^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11})$";
+    public const string cvvPattern = @"^\d{3}$";
+    public const string expirationDatePattern = @"^(0[1-9]|1[0-2])\/\d{2}$"; 
+    public const string cardHolderNamePattern = @"^[A-Za-z\s]{2,50}$";
+}
