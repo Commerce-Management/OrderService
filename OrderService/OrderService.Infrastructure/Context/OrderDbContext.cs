3cbdcb23104c52e5ea56c90f8e54eb5323f08fff
Ziya Hacili <magahajili@gmail.com>
2025-10-26 11:41:01 +0400

    db context

---
+++ OrderService/OrderService.Infrastructure/Context/OrderDbContext.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using OrderService.Core.Entities;
+
+namespace OrderService.Infrastructure.Context;
+
+public class OrderDbContext : DbContext
+{
+    public OrderDbContext()
+    {
+    }
+
+    public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+    {
+    }
+    
+    public virtual DbSet<Order> Orders { get; set; }
+    public virtual DbSet<OrderItem> OrderItems { get; set; }
+    public virtual DbSet<PaymentTransaction> PaymentTransactions { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrderDbContext).Assembly);
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+      
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())  
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+    
+    
+}
